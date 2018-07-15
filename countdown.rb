99e938d8f4b8075cb6d1ee3fd117cda31c20501c
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-07-15 01:47:06 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,17 @@ def countdown(counter)
  "HAPPY NEW YEAR!"
   
 end
+
+
+def countdown_with_sleep(counter)
+  
+    seconds = counter 
+  
+  while  seconds > 0  
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1 
+  end
+
+ "HAPPY NEW YEAR!"
+  
+end
